672a78312fdfb04ec66f3465a9e0462ff7a5f6d2
Kaustubh Bhargava <kaustubhbhargava5@gamil.com>
2023-01-08 23:45:53 +0530

    8th Jan 23

---
+++ Goldman Sachs/Max-points-on-the-line.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+def maxPoints(points) -> int:       
+        if len(points)<=2: return len(points)
+
+        def line(p1,p2):
+            # Vertical line
+            if p2[0]-p1[0] == 0:
+                return (p1[0])
+            slope = (p2[1]-p1[1]) / (p2[0]-p1[0])                
+            b = p1[1] - slope * p1[0]
+            return (slope,"%.5f" % b)
+        res = 0
+
+        for i in range(len(points)):
+            count = defaultdict(int)
+            for j in range(i+1,len(points)):
+                count[line(points[i],points[j])] += 1
+            if count:
+                res = max(res,max(count.values()))
+        return res + 1
---
+++ Goldman Sachs/Minimum-consicutive-cards-to-pick-up.py
@@ -0,0 +1,13 @@
+def minimumCardPickup(cards) -> int:
+        HashMap={}
+        lengths=[]
+        for i,j in enumerate(cards):
+            if j in HashMap:
+                lengths.append(abs(HashMap[j]-i)+1)
+                HashMap[j]=i
+            else:
+                HashMap[j]=i
+        if len(lengths)>0:
+            return min(lengths)
+        else:
+            return -1
